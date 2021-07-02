cb0abcdc56afd6bf1040e243845597c255290a5e
lordvidex <lordvidex.prime@gmail.com>
2021-07-02 07:52:20 +0300

    Initial Commit

---
+++ Assignment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by Evans Owamoyo on 02.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
